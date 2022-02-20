f424af1c655c9adb1e393b8a428a59c656c61a72
Ivor Cvetkovski <56237355+CvetkovskiIvor@users.noreply.github.com>
2022-02-20 01:33:14 +0100

    Add files via upload

--- URS_projekt/URS_projekt/main.c
+++ URS_projekt/URS_projekt/main.c
@@ -1,56 +1,36 @@
-#define F_CPU 7372800UL			/* Define frequency here its 8MHz */
+#define F_CPU 7372800UL
+#include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-//#define USART_BAUDRATE 9600
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
-
-
-void UART_init(long USART_BAUDRATE)
-{
-	UCSRB |= (1 << RXEN) | (1 << TXEN);/* Turn on transmission and reception */
-	UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);/* Use 8-bit character sizes */
-	UBRRL = BAUD_PRESCALE;		/* Load lower 8-bits of the baud rate value */
-	UBRRH = (BAUD_PRESCALE >> 8);	/* Load upper 8-bits*/
-}
-
-unsigned char UART_RxChar()
-{
-	while ((UCSRA & (1 << RXC)) == 0);/* Wait till data is received */
-	return(UDR);			/* Return the byte*/
-}
-
-void UART_TxChar(char ch)
-{
-	while (! (UCSRA & (1<<UDRE)));	/* Wait for empty transmit buffer*/
-	UDR = ch ;
-}
-
-void UART_SendString(char *str)
-{
-	uint8_t j=0;
-	
-	while (str[j]!=0)		/* Send string till null */
-	{
-		UART_TxChar(str[j]);	
-		j++;
-	}
-}
 
 int main()
 {
-	//char c;
-	UART_init(4800);
+	int i = 0;
+	unsigned int ubrr = 23;
+	unsigned char data[] = "MMMM";
 	
-	//UART_SendString("\n\t Echo Test ");	
-	while(1)
-	{
-		//c=UART_RxChar();
-		//UART_TxChar('c');
-		UART_SendString("Hi");
+	UBRRH = (ubrr>>8); 
+	UBRRL = (ubrr);    
+	
+	UCSRC |= (1 << URSEL) | (1 << UCSZ1) | (1 << UCSZ0);       
+	UCSRB |= (1 << TXEN); 
+	
+	while(1) {
+		i = 0;
+		while(data[i] != 0) 
+		{
+			while (!( UCSRA & (1<<UDRE))); 
+			
+			UDR = data[i];	
+			i++;                             
+		}
+		
+		while (!( UCSRA & (1<<UDRE)));   
+		UDR = '\n';			
+		while (!( UCSRA & (1<<UDRE))); 
+		UDR = '\r';
 		
 		_delay_ms(1000);
-	}	
+	}
+	
 }
