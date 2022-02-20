de8e4cf23d927b5fd0d5ed797a6f0114196eb6c7
Ivor Cvetkovski <56237355+CvetkovskiIvor@users.noreply.github.com>
2022-02-20 03:39:01 +0100

    Add files via upload

--- URS_projekt/URS_projekt/main.c
+++ URS_projekt/URS_projekt/main.c
@@ -5,17 +5,21 @@
 
 int main()
 {
+	DDRA = 0xff;
+	//PORTA = 0;
+	//_delay_ms(1000);
 	int i = 0;
-	unsigned int ubrr = 23;
-	unsigned char data[] = "MMMM";
+	unsigned int ubrr = 95;
+	unsigned char data[] = "c";
 	
-	UBRRH = (ubrr>>8); 
-	UBRRL = (ubrr);    
+	UBRRH = (ubrr>>8);
+	UBRRL = (ubrr);  
 	
-	UCSRC |= (1 << URSEL) | (1 << UCSZ1) | (1 << UCSZ0);       
+	UCSRC |= (1 << URSEL) | (1 << UCSZ1) | (1 << UPM1) | (1 << UPM0) | (1 << USBS);       
 	UCSRB |= (1 << TXEN); 
 	
 	while(1) {
+		
 		i = 0;
 		while(data[i] != 0) 
 		{
@@ -25,10 +29,11 @@ int main()
 			i++;                             
 		}
 		
-		while (!( UCSRA & (1<<UDRE)));   
-		UDR = '\n';			
-		while (!( UCSRA & (1<<UDRE))); 
-		UDR = '\r';
+		//while (!( UCSRA & (1<<UDRE)));   
+		//UDR = '\n';			
+		//while (!( UCSRA & (1<<UDRE))); 
+		//UDR = '\r';
+		
 		
 		_delay_ms(1000);
 	}
