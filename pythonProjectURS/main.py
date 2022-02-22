c1ef67092c78f570f02b9b4fdb936cf4d0da6bb9
Ivor Cvetkovski <56237355+CvetkovskiIvor@users.noreply.github.com>
2022-02-22 23:12:20 +0100

    Finalni python kod
    Dodan time monitoring, te provjera je li kartica u sustavu dozvoljenih kartica(usput se korigira greska prilikom prijenosa)

--- pythonProjectURS/main.py
+++ pythonProjectURS/main.py
@@ -1,9 +1,10 @@
-import serial, time
+from datetime import datetime
+import serial
 
 ser = serial.Serial()
 ser.port = "/dev/ttyUSB0"
 ser.baudrate = 4800
-ser.bytesize = serial.SIXBITS
+ser.bytesize = serial.SEVENBITS
 ser.parity = serial.PARITY_ODD
 ser.stopbits = serial.STOPBITS_TWO
 
@@ -13,16 +14,38 @@ ser.xonxoff = False
 ser.rtscts = False
 ser.dsrdtr = False
 
+cardArray = ["E45A681EC8", "8234547391", "804581C387", "91AA4E7306", "5302F3C361", "9C3B527386", "B7324373B5",
+             "B7324373B5"]
+activeCards = [-1]*8
+
 try:
     ser.open()
 
 except Exception as e:
-    print ("error open serial port: " + str(e))
+    print("error open serial port: " + str(e))
     exit()
 
 if ser.isOpen():
     while True:
-        read_data = ser.read(6)
-        print("data recieved: " + read_data.decode('utf-8'))
+        read_data = ser.read(100)
+        serialOut = read_data.decode('utf-8')
+        print(serialOut)
+        if serialOut in cardArray:
+            now = datetime.now()
+            currTime = now.strftime("%H:%M:%S")
+            print("Card detected!")
+            index = cardArray.index(serialOut)
+
+            activeCards[index] *= -1
+
+            if activeCards[index] == 1:
+                print("Card with id: " + serialOut + " entered at " + currTime)
+            else:
+                print("Card with id: " + serialOut + " exited at " + currTime)
+        elif not serialOut:
+            print("No card detected!")
+        else:
+            print("Error while reading card!")
+
 else:
     print("Can not open serial port")
